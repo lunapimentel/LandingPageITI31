8673024574e95c71e1fc0144dc0690e714d77f3a
lunapimentel <lunarpimentel03@gmail.com>
2026-06-08 10:11:19 -0300

    Inicio do projeto

---
+++ modulos/rodape.php
@@ -0,0 +1,7 @@
+<footer style="margin-top: 50px;">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> Luna Rocha. Todos os direitos reservados.</p>
+    </div>
+</footer>
+</body>
+</html>
