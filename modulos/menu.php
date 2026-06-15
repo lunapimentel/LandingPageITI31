76fb4cf810934116660e0cba4e38bcf44deb207b
lunapimentel <lunarpimentel03@gmail.com>
2026-06-15 10:33:37 -0300

    adicionado o menu e curriculo

--- modulos/menu.php
+++ modulos/menu.php
@@ -1,11 +1,13 @@
 <nav class="nav container">
     <div class="nav-left">
-        <a class="brand" href="#">Logotipo</a>
+        <a class="brand" href="#">Luna Rocha</a>
     </div>
     <div class="nav-right">
         <a href="#home" class="tabs">Início</a>
         <a href="#recursos" class="tabs">Recursos</a>
         <a href="#contato" class="tabs">Contato</a>
+        <a href="#curriculo" class="tabs">Currículo</a>
+        <a href="#portfolio" class="tabs">Portfolio</a>
     </div>
 </nav>
 <hr>
